c9afde4e967e0c66ef242bad889507c3581efdf3
rporeitere <62703472+vinnijs@users.noreply.github.com>
2022-03-05 22:13:26 +0200

    Fixed map pixelation; Fixed map background image

--- helper_functions/pixelate-map.py
+++ helper_functions/pixelate-map.py
@@ -1,5 +1,7 @@
 from PIL import Image
 
+#to test - put in file path values
+
 width = 1000
 height = 1000
 pixelWidth = 5
@@ -7,13 +9,13 @@ pixelHeight = 5
 nOfPixelsX = (int)(width/pixelWidth)
 nOfPixelsY = (int)(height/pixelHeight)
 
-originalPicturePath = "images\map\dark-map.png"
-pixelatedPicturePath = "images\map\base-map.png"
+originalPicturePath = "dark-map.png" 
+pixelatedPicturePath = "base-map.png"
 
 #creates a pixelated map of the rally teritory for as a base
-#takes dark-map.png and resizes it to needed amount of pixels. then saves it to base-map.png
+#takes Original Picture and resizes it to needed amount of pixels. then saves it to Pixelated Picture
 
 img = Image.open(originalPicturePath)
 imgSmall = img.resize((nOfPixelsX,nOfPixelsY),resample=Image.BILINEAR)
-result = imgSmall.resize(img.size,Image.NEAREST)
+result = imgSmall.resize((width,height), Image.LANCZOS)
 result.save(pixelatedPicturePath)
