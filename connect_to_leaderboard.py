7b8c60862d37b144919806164135ec174494cdad
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-03-27 00:59:35 +0200

    leaderboard_database

--- connect_to_leaderboard.py
+++ connect_to_leaderboard.py
@@ -1,9 +1,13 @@
 import firebase_admin
 from firebase_admin import db
-import json
+import json, os
 
 def upload_result(name,  points, time):
-    cred_obj = firebase_admin.credentials.Certificate('leaderboard_database/lapoleaderboard-firebase-adminsdk-shvog-50b79db982.json')
+    with open('leaderboard_database/lapoleaderboard-firebase-adminsdk-shvog-0bbe9e7049.json') as f:
+        cert = json.load(f)
+
+    cert["private_key"] = os.environ["PRIVATE_KEY"]
+    cred_obj = firebase_admin.credentials.Certificate(cert)
     default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL': 'https://lapoleaderboard-default-rtdb.europe-west1.firebasedatabase.app/'})
     ref = db.reference("/")
     res = {
